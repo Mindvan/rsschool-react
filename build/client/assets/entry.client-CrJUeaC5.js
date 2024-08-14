697fbbdf0a7e4fc3dfecfd9e7b361e4f2c6b706b
Ivan Eroshin <itseroshin@gmail.com>
2024-08-14 08:00:00 +0300

    feat: smth

--- build/client/assets/entry.client-CrJUeaC5.js
+++ build/client/assets/entry.client-CrJUeaC5.js
@@ -1,4 +1,4 @@
-import{E as y,c as g,r as t,i as E,d as F,a as $,m as S,s as k,b as O,e as P,f as H,g as j,h as B,u as D,R as L,j as z,k as A,l as T,n as p}from"./components-De1Nnuln.js";/**
+import{E as y,c as g,r as t,i as E,d as F,a as $,m as S,s as k,b as O,e as P,f as H,g as j,h as B,u as D,R as L,j as z,k as A,l as T,n as p}from"./components-BzD752jl.js";/**
  * @remix-run/react v2.11.1
  *
  * Copyright (c) Remix Software Inc.
---
+++ build/client/build/manifest-98AEFC5B.js
@@ -0,0 +1,1 @@
+window.__remixManifest={"entry":{"module":"/build/entry.client-DLNFUCHL.js","imports":["/build/_shared/chunk-K5QFEYVH.js"]},"routes":{"root":{"id":"root","path":"","module":"/build/root-J4GBOWM6.js","hasAction":false,"hasLoader":false,"hasClientAction":false,"hasClientLoader":false,"hasErrorBoundary":false}},"version":"98aefc5b","url":"/build/manifest-98AEFC5B.js"};
